2f1151506ebf055b5ba54731e6456cd54231c472
xxxx24048 <102151407+xxxx24048@users.noreply.github.com>
2022-07-15 15:17:47 +0200

    nth

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,23 +1,20 @@
 const multer = require("multer");
 
-// Types of files accepted
 const MIME_TYPES = {
   "image/jpg": "jpg",
   "image/jpeg": "jpg",
   "image/png": "png",
 };
 
-// Configure multer - creates a storage object
+
 const storage = multer.diskStorage({
-  //Setting the destination of the file (images folder)
-  destination: (req, file, cb) => {
-    cb(null, "images");
+  destination: (req, file, callback) => {
+    callback(null, "images");
   },
-  //Setting the name of the file, using the original name of the file, spaces replaced by underscores + timestamp
-  filename: (req, file, cb) => {
+  filename: (req, file, callback) => {
     const name = file.originalname.split(" ").join("_");
     const extension = MIME_TYPES[file.mimetype];
-    cb(null, name + Date.now() + "." + extension);
+    callback(null, name + Date.now() + "." + extension);
   },
 });
 
